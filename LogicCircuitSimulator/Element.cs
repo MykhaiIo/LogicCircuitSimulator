23f9f1bccb65068d0eb822af251bea0086f346a8
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2019-01-06 00:14:24 +0200

    Added Element and Terminal classes

---
+++ LogicCircuitSimulator/Element.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCircuitSimulator
+{
+    abstract class Element
+    {
+        public List<Pin> input_pins, output_pins;
+
+        public int Delay { get; set; }
+        public abstract void Functionality();
+    }
+
+    abstract class Terminal : Element
+    {
+        
+    }
+
+    class InTerminal : Terminal
+    {
+        public bool SimulationInputValue { get; set; }
+
+        public InTerminal()
+        {
+            input_pins.Clear();
+            output_pins.Add(new Pin());
+            Delay = 0;
+        }
+
+        public override void Functionality()
+        {
+            output_pins[0].State = SimulationInputValue;
+        }
+    }
+
+    class OutTerminal : Terminal
+    {
+        public bool SimulationResult { get; private set; }
+
+        public OutTerminal()
+        {
+            input_pins.Add(new Pin());
+            output_pins.Clear();
+            Delay = 0;
+        }
+
+        public override void Functionality()
+        {
+            SimulationResult = input_pins[0].State;
+        }
+    }
+}
