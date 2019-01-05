7ebbe1faa39c53f0596e6e6cebdc310582ab9588
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2019-01-06 00:13:21 +0200

    Added Pin class

---
+++ LogicCircuitSimulator/Pin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicCircuitSimulator
+{
+    class Pin
+    {
+        public Pin()
+        {
+            State = false;
+        }
+
+        public bool State { get; set; }
+    }
+}
