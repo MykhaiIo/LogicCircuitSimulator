8b5c8f8c9237dc33ca5fce5f96c273168a89ef06
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2019-01-06 13:58:49 +0200

    Added NAND, OR, NOR, XOR, XNOR, NOT, BUFF gates

--- LogicCircuitSimulator/Element.cs
+++ LogicCircuitSimulator/Element.cs
@@ -55,6 +55,25 @@ namespace LogicCircuitSimulator
 
     abstract class Gate : Element
     {
+        protected void Initialize1to1AsynchGate()
+        {
+            input_pins.Add(new Pin());
+            output_pins.Add(new Pin());
+            Delay = 0;
+        }
+
+        protected void Initialize2to1AsynchGate()
+        {
+            Initialize1to1AsynchGate();
+            input_pins.Add(new Pin());
+        }
+
+        protected void Initialize1to2AsynchGate()
+        {
+            Initialize1to1AsynchGate();
+            output_pins.Add(new Pin());
+        }
+
         // TODO: Add methods for number of pins
     }
 
@@ -62,10 +81,7 @@ namespace LogicCircuitSimulator
     {
         public AND()
         {
-            input_pins.Add(new Pin());
-            input_pins.Add(new Pin());
-            output_pins.Add(new Pin());
-            Delay = 0;
+            Initialize2to1AsynchGate();
         }
 
         public override void Functionality()
@@ -77,6 +93,130 @@ namespace LogicCircuitSimulator
             }
 
             output_pins[0].State = rv;
+        }
+    }
+
+    class NAND : Gate
+    {
+        public NAND()
+        {
+            Initialize2to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            Logic rv = new Logic(LogicValue.LOGIC_1);
+            foreach (Pin ip in input_pins)
+            {
+                rv &= ip.State;
+            }
+
+            output_pins[0].State = !rv;
+        }
+    }
+
+    class OR : Gate
+    {
+        public OR()
+        {
+            Initialize2to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            Logic rv = new Logic(LogicValue.LOGIC_0);
+            foreach (Pin ip in input_pins)
+            {
+                rv |= ip.State;
+            }
+
+            output_pins[0].State = rv;
+        }
+    }
+
+    class NOR : Gate
+    {
+        public NOR()
+        {
+            Initialize2to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            Logic rv = new Logic(LogicValue.LOGIC_0);
+            foreach (Pin ip in input_pins)
+            {
+                rv |= ip.State;
+            }
+
+            output_pins[0].State = !rv;
+        }
+    }
+
+    class XOR : Gate
+    {
+        public XOR()
+        {
+            Initialize2to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            Logic rv = new Logic(LogicValue.LOGIC_0);
+            foreach (Pin ip in input_pins)
+            {
+                rv ^= ip.State;
+            }
+
+            output_pins[0].State = rv;
+        }
+    }
+
+    class XNOR : Gate
+    {
+        public XNOR()
+        {
+            Initialize2to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            Logic rv = new Logic(LogicValue.LOGIC_0);
+            foreach (Pin ip in input_pins)
+            {
+                rv ^= ip.State;
+            }
+
+            output_pins[0].State = !rv;
+        }
+    }
+
+    class NOT : Gate
+    {
+        public NOT()
+        {
+            Initialize1to1AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            output_pins[0].State = !input_pins[0].State;
+        }
+    }
+
+    class BUFF : Gate
+    {
+        public BUFF()
+        {
+            Initialize1to2AsynchGate();
+        }
+
+        public override void Functionality()
+        {
+            foreach (Pin op in output_pins)
+            {
+                op.State = input_pins[0].State;
+            }
         }
     }
 }
