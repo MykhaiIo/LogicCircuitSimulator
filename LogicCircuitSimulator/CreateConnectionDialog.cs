77a1d99daccb82bdd7994ef5b0fec0edecfeb39e
MykhaiIo <43903023+MykhaiIo@users.noreply.github.com>
2019-01-10 03:07:30 +0200

    CreateConnectionDialog form added request (#13)
    * Added dialogs and changed main form
    
    * Added port and gate dialogs forms
    
    * Changed main form, added files for GUI
    
    * Current changes
    
    * Added drag-n-droppable element (AND) picture
    
    * Added Visual.MultiInputGates implementation and implemented methods to draw connection between two pins
    
    * Revert "Added Visual.MultiInputGates implementation and implemented methods to draw connection between two pins"
    
    This reverts commit b1e8cfe02abe92341b6c5ab0a02746c20c30cefe.
    
    * Deleted VisualElementDeprecated added methods to draw connections
    
    * Fixed main form bug
    
    * CreateConnectionDialog form added

--- LogicCircuitSimulator/CreateConnectionDialog.cs
+++ LogicCircuitSimulator/CreateConnectionDialog.cs
@@ -15,6 +15,17 @@ namespace LogicCircuitSimulator
         public CreateConnectionDialog()
         {
             InitializeComponent();
+
+            foreach (var elem in GUI.elements)
+            {
+                listBox1.Items.Add(elem.Identificator);
+                listBox2.Items.Add(elem.Identificator);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
